5bcd64f78c5b4669fe8d92bce14d29de75d0b471
36t <saluta@gmail.com>
2020-12-09 14:30:03 +0900

    feat: created FrameworkList.tsx.

---
+++ src/component/FrameworkList.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+type Framework = {
+  id: number
+  item: string
+}
+
+type Props = {
+  frameworks: Framework[]
+}
+
+const FrameworkList: React.FC<Props> = (list: Props) => {
+  if (!list.frameworks || !list.frameworks.length) {
+    return <h1>データがありません</h1>
+  }
+
+  return (
+    <div>
+      <ul>
+        {list.frameworks.map(({ id, item }) => (
+          <li key={id}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default FrameworkList
