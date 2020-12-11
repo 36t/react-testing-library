b19e33f7e94e65173495ac53ba21caf10e9fbebf
36t <saluta@gmail.com>
2020-12-11 17:01:33 +0900

    feat: added load username logic and updated jsx syntax.

--- src/component/ReduxAsync.tsx
+++ src/component/ReduxAsync.tsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { selectCount, loadNumber } from 'features/customCounter/customCounterSlice'
+import { selectCount, selectUsername, loadNumber, loadUsername } from 'features/customCounter/customCounterSlice'
 
 const ReduxAsync: React.FC = () => {
   const count = useSelector(selectCount)
+  const username = useSelector(selectUsername)
+
   const dispatch = useDispatch()
 
   return (
     <div>
-      <span data-testid="count-value">{count}</span>
-      <button onClick={() => dispatch(loadNumber(5))}>LoadNumber</button>
+      <div>
+        <p data-testid="count-value">{count}</p>
+        <button onClick={() => dispatch(loadNumber(5))}>LoadNumber</button>
+      </div>
+      <div>
+        {username && <h1>{username}</h1>}
+        <button onClick={() => dispatch(loadUsername())}>LoadUsername</button>
+      </div>
     </div>
   )
 }
