7389454903fb0e10c9d0be2acbe86664c6491cad
36t <saluta@gmail.com>
2020-12-11 17:02:56 +0900

    mod: fixed extention to ts from tsx.

---
+++ src/service/service.ts
@@ -0,0 +1,36 @@
+import axios from 'axios'
+
+type AddressType = {
+  street: string
+  suite: string
+  city: string
+  zipcode: string
+}
+
+type CompanyType = {
+  name: string
+  catchPhrase: string
+  bs: string
+}
+
+export type UserType = {
+  id: number
+  name: string
+  username: string
+  email: string
+  address: AddressType
+  phone: string
+  website: string
+  company: CompanyType
+}
+
+export const fetchUser = async (): Promise<UserType> => {
+  try {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/users/1')
+    return response.data as UserType
+  } catch (error) {
+    return error.message
+    // return Promise.reject(error): rejectされるだけで処理がここで止まる
+    // throw new Error(error): コンパイルエラーでそもそも使えない
+  }
+}
