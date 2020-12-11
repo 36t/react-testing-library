0bc40b9042626283a4d864ee2e9bdf1038e400d4
36t <saluta@gmail.com>
2020-12-11 22:42:27 +0900

    refactor: fixed fetchUser function logic.

--- src/service/service.ts
+++ src/service/service.ts
@@ -24,13 +24,29 @@ export type UserType = {
   company: CompanyType
 }
 
-export const fetchUser = async (): Promise<UserType> => {
+export type ResponseType = {
+  data: UserType
+}
+
+export const fetchUser = async (): Promise<ResponseType> => {
+  /*
   try {
-    const response = await axios.get('https://jsonplaceholder.typicode.com/users/1')
+    // const response = await axios.get('https://jsonplaceholder.typicode.com/users/1')
+    const response = await axios.get('https://jsonplaceholder.typicode.com/error/501')
     return response.data as UserType
   } catch (error) {
-    return error.message
+    // return error.message
+    throw new Error(error.message)
     // return Promise.reject(error): rejectされるだけで処理がここで止まる
     // throw new Error(error): コンパイルエラーでそもそも使えない
   }
+  */
+
+  return await axios.get('https://jsonplaceholder.typicode.com/users/1')
+
+  // if (response.status !== 200) {
+  //   throw new Error(`Error: status code ${response.status}, status text ${response.statusText}`)
+  // }
+
+  // return response.data as UserType
 }
