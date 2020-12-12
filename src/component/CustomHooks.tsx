0918f6f36e42c51b29f0557a221fb3a0a0491ac1
36t <saluta@gmail.com>
2020-12-12 21:32:42 +0900

    feat: created CustomHooks component.

---
+++ src/component/CustomHooks.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useCounter } from 'hoooks/useCounter'
+
+const CustomHooks: React.FC = () => {
+  // カスタムHooks
+  const { count, increment, decrement, double, triple, reset } = useCounter(3)
+
+  return (
+    <div>
+      <p>{count}</p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>decrement</button>
+      <button onClick={double}>Double</button>
+      <button onClick={triple}>Triple</button>
+      <button onClick={reset}>Reset</button>
+    </div>
+  )
+}
+
+export default CustomHooks
